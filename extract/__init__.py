f0928b1a33b68754821199f34f63a78c9dfd2c06
✨小透明・宸✨ <1272742391@qq.com>
2021-03-15 09:40:25 +0800

    在不能提取brand时使用brand或title作为fallback

--- extract/__init__.py
+++ extract/__init__.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import re
+import warnings
 from .brandRegex import brandRegex
 
 def brand(s: pd.core.series.Series) -> str:
-    data = s['title'] if pd.isna(s['brand']) else s['brand'] # type: str
-    for key, regex in brandRegex.items():
-        key # type: str
-        regex # type: re.Pattern
+    for key, regex in brandRegex.items(): # type: str, re.Pattern
         if (not pd.isna(s['brand']) and regex.search(s['brand'])) or regex.search(s['title']):
             return key
-    raise Exception(f'Failed to extract brand: {data}')
+    fallback = (s["title"] if pd.isna(s["brand"]) else s["brand"]).lower()
+    warnings.warn(f'Unable to extract brand, fallback to "{fallback}".')
+    return fallback
