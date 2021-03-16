f2cfedf685a7801cb66be1336af95212159d2aaa
张凯薇 <55012529+Vicinity111@users.noreply.github.com>
2021-03-16 21:32:43 +0800

    Update regexPattern.py

--- extract/regexPattern.py
+++ extract/regexPattern.py
@@ -17,3 +17,5 @@ weight = re.compile(r'\b([0-9]*\.?[0-9]+) ?((?:kg|pounds|lbs))\b')
 disk = re.compile(r'\b([0-9]+) ?((?:gb|tb))(?: (?:\d+ ?rpm ?)?(ram|hdd|sata|mechanical_hard_drive|hard drive|ssd|flash_memory_solid_state))?(?: \/ \d+ ?rpm)?\b')
 
 cpuBrand=re.compile(r'(intel|amd|core)(\s[a-z]+\s(a|i)[0-9]|\s[a-z]+[0-9]|\s(e|a)(-|\s)([0-9]+|[a-z]+)|\s[a-z]+|)')
+
+cpuFrequency=re.compile(r'[0-9](.|)([0-9]+|)(\s|)(ghz|mhz)')
