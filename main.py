36ed0d83b286934730ba9a6d40c282858f6814e2
张凯薇 <55012529+Vicinity111@users.noreply.github.com>
2021-03-16 18:49:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ if __name__ == '__main__':
 
     data = pd.read_csv('datasets/X2.csv', dtype=pd.StringDtype())
     data['x_brand'] = pd.Series(dtype=pd.StringDtype())
-    data['x_weight'] = pd.Series(dtype=pd.Float32Dtype())
+    data['x_weight'] = pd.Series(dtype=pd.Float32Dtype()) # pandas=1.0.0报错，我运行的时候删了，默认应该就是float64
+    data['x_cpuBrand'] = pd.Series(dtype=pd.StringDtype())
 
     # 把所有数据转为小写
     for index, row in data.iterrows():
@@ -19,8 +20,10 @@ if __name__ == '__main__':
     for index, row in data.iterrows():
         data.loc[index, 'x_brand'] = extract.brand(row)
         data.loc[index, 'x_weight'] = extract.weight(row)
+        data.loc[index,'x_cpuBrand']=extract.cpuBrand(row)
 
     print(data)
-
+    #data.to_csv('test.csv')
+    
     te = time.perf_counter()
     print(te - ts)
