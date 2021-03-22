c316c9c3a0eda5a9f3db1a7813602eb5c9396e21
张凯薇 <55012529+Vicinity111@users.noreply.github.com>
2021-03-22 10:12:18 +0800

    Update __init__.py

--- compare/__init__.py
+++ compare/__init__.py
@@ -32,7 +32,7 @@ def notebook(seriesA: pd.Series, seriesB: pd.Series) -> bool:
         if (seriesA['x_ram_type'] not in seriesB['x_ram_type']) and (seriesB['x_ram_type'] not in seriesA['x_ram_type']):
             return False
         
-    for colVeto in ('x_cpu_frequency', 'x_ram_capacity', 'x_model'):
+    for colVeto in ('x_cpu_frequency',  'x_model'):   # 'x_ram_capacity'
         if all((
             not pd.isna(seriesA[colVeto]),
             not pd.isna(seriesB[colVeto]),
